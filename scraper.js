8e1d54c51ef9b39fb3ef84dc638488ee19fcdb5c
Dawid <dpiechotaa@gmail.com>
2021-05-05 00:20:40 +0200

    scrape all links. Log the process nicely

--- scraper.js
+++ scraper.js
@@ -2,45 +2,45 @@ import axios from "axios";
 import puppeteer from "puppeteer";
 import fs from 'fs';
 const url = "https://vipp.com/en/api/products?";
-// Flag for testing ( True: 22 products, False: all of them ).
-const testing = false;
+const testing = {
+  browser: false, // false: headless   | true: visual browser
+  links: true     // false: all links  | true: first 5 links
+};
 
-// //const reqLimit = testing ? 1 : 150;
-// const reqLimit = 1;
+const reqLimit = testing.links ? 1 : 150;
+const reqIncrement = testing.links ? 5 : 22;
 
-// let links = [];
-// const promises = [];
+let links = [];
+const promises = [];
 
-// for (let i = 0; i < reqLimit; i+=22) {
-//   promises.push(axios.get(`${url}start=${i}&amount=${22}`));
-// }
-// try {
-//   for ( const el of await Promise.all(promises) ) {
-//     links.push(...el.data.products);
-//   }
-// } catch (error) {
-//   console.error(error.message);
-//   console.log("Something went wrong while fetching links from vipp api");
-//   process.exit(1);
-// }
+for (let i = 0; i < reqLimit; i+=reqIncrement) {
+  promises.push(axios.get(`${url}start=${i}&amount=${reqIncrement}`));
+}
+try {
+  for ( const el of await Promise.all(promises) ) {
+    links.push(...el.data.products);
+  }
+} catch (error) {
+  console.error(error.message);
+  console.log("Something went wrong while fetching links from vipp api");
+  process.exit(1);
+}
 
-// links = links.map(el => `https://vipp.com${el.link}`);
-// console.log(`Products to scrape: (${links.length}):`);
-// links.forEach(el => console.log(el));
+links = links.map(el => `https://vipp.com${el.link}`);
+console.log(`Products to scrape: (${links.length}):`);
 
 // ------------------------------------
 // Puppeteer
 // ------------------------------------
 
 const scrapePage = async(page, pageLink) => {
-  console.log(`Scraping: ${pageLink}`);
   await page.goto(pageLink);
   
   await page.addStyleTag({ content: "* {scroll-behavior: auto !important;}" });
   // This was difficult to fix. Smooth scrolling *sometimes* prevents clicks on elements
 
   const productInfo = await page.evaluate(() => {
-    const data = {images: [], colors: [], details: []};
+    const data = {colors: [], details: []};
     // ---------------------------
     // MAIN INFO
     // ---------------------------
@@ -67,35 +67,31 @@ const scrapePage = async(page, pageLink) => {
     // ---------------------------
     // COLORS
     // ---------------------------
-    //const availableColors = document.getElementsByClassName("dropdown-list")[0].children[0].innerText;
-    const availableColors = document.getElementsByClassName("dropdown-list")[0].children;
-    for( color of availableColors ) {
-      data.colors.push(color.innerText);
+    const availableColors = document.getElementsByClassName("dropdown-list")[0]?.children;
+    if(availableColors !== undefined) {
+      for( color of availableColors ) {
+        data.colors.push(color.innerText);
+      }
+    } else {
+      data.colors.push("-");
     }
     return data;
   })
   // ---------------------------
   // IMAGE LINKS
   // ---------------------------
-  productInfo.images2 = [];
+  productInfo.images = [];
   for( let i = 1; i < productInfo.colors.length+1; i++) {
-    //await page.waitForTimeout(2000);
-    //const hrefElement  = await page.$("label > span");
-    //console.warn(hrefElement);
-    //await hrefElement.click();
-    //await page.waitForSelector("label > span");
-    await page.click("label > span");
-    console.log("label clicked");
-    //await page.waitForTimeout(5000);
-    console.log("color clicked");
-    const colorClick = page.click(`ul.dropdown-list > li:nth-child(${i})`);
-    const response = page.waitForResponse(response => response.url() === "https://vipp.com/en/system/ajax" && response.status() === 200 );
-    await Promise.all([
-      colorClick,
-      response
-    ]);
-    console.log("color loaded");
-    await page.waitForTimeout(500);
+    if( productInfo.colors.length !== 1 ) { // case of no color options
+      await page.click("label > span");
+      const colorClick = page.click(`ul.dropdown-list > li:nth-child(${i})`);
+      const response = page.waitForResponse(response => response.url() === "https://vipp.com/en/system/ajax" && response.status() === 200 );
+      await Promise.all([
+        colorClick,
+        response
+      ]);
+      await page.waitForTimeout(500);
+    }
 
     const newImages = await page.evaluate(() => {
       const images = [];
@@ -104,21 +100,29 @@ const scrapePage = async(page, pageLink) => {
       }
       return images;
     });
-    productInfo.images2.push(...newImages);
+    productInfo.images.push(...newImages);
+    console.log(`\t[${i}/${productInfo.colors.length}] Image links scraped.`);
   }
     
   return productInfo;
 }
 
-console.log("Start");
-const browser = await puppeteer.launch( testing ? {
+console.log("Starting browser");
+const browser = await puppeteer.launch( testing.browser ? {
   headless: false,
   slowMo: 250, // 250ms delay
 } : {});
 const page = await browser.newPage();
-const link = 'https://vipp.com/en/products/table-lamp';
+// const link = 'https://vipp.com/en/products/table-lamp';
 
-const productInfo =  await scrapePage(page, link);
+const allScrapedProducts = [];
+for( let i = 0; i < links.length; i++ ) {
+  console.log(`[${i+1}/${links.length}] Scraping: ${links[i]}`);
+  const productInfo = await scrapePage(page, links[i]);
+  allScrapedProducts.push(productInfo);
+}
+//const productInfo2 =  await scrapePage(page, "https://vipp.com/en/products/pedal-bin-4-l-1-gal");
+
 
 await browser.close();
 
@@ -129,14 +133,11 @@ await browser.close();
 
 async function downloadImages(imageArray, productSku, imgDir) {
   for (let i = 0; i < imageArray.length; i++) {
-    //const response = await fetch(imageArray[i]);
-    //const buffer = await response.buffer();
     const response = await axios.get(imageArray[i],{
       responseType: 'arraybuffer'
     })
-    //fs.writeFile(`./images/${productSku}_${i}.jpg`, buffer, () => 
     fs.writeFile(`./${imgDir}/${productSku}_${i}.jpg`, response.data, () => 
-      console.log(`Downloaded ${imageArray[i]}`));
+      console.log(`\tDownloaded ${imageArray[i]}`));
   }
 }
 
@@ -145,13 +146,18 @@ if (!fs.existsSync(imgDir)){
     fs.mkdirSync(imgDir);
 }
 
-await downloadImages(productInfo.images2, "VIPP42", imgDir);
+for(let i = 0; i < allScrapedProducts.length; i++) {
+  console.log(`[${i+1}/${allScrapedProducts.length}] Downloading images of: ${allScrapedProducts[i].sku}`);
+  await downloadImages(allScrapedProducts[i].images, allScrapedProducts[i].sku, imgDir);
+}
+//await downloadImages(productInfo2.images, "VIPP_XD", imgDir);
 
 // ---------------------------
 // DATA PARSING
 // ---------------------------
 
-const saveAsJson = (data) => fs.writeFileSync('data.json', JSON.stringify(data, null, 4));
+const saveAsJson = (data, filename) => fs.writeFileSync(`${filename}.json`, JSON.stringify(data, null, 4));
 
 
-saveAsJson(productInfo);
+saveAsJson(allScrapedProducts, "allScrapedProducts");
+//saveAsJson(productInfo2, "data2");
